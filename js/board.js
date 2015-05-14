40904b4d2ca56ecf983da56259aca12ad9d02468
Eric <e.mandersloot@gmail.com>
2015-05-14 23:37:47 +0200

    intiated the board

--- js/board.js
+++ js/board.js
@@ -1,5 +1,57 @@
+/*=============================================================
+---------------------------------------------------------------
+===============================================================
+
+	Board constructor:
+	Creates and initiates the battlefield.
+
+===============================================================	
+---------------------------------------------------------------
+=============================================================*/
+
 var BOARD = (function( $ ){
 
-	alert('jo');
+	var $battlefield 	= $("#battlefield"),
+		Battlefield 	= [];
+
+	var Field = function( y, x, id ){
+
+		this.row 		= y;
+		this.column 	= x;
+		this.id 		= "row" + y + "column" + x;
+		this.occupied	= false;
+		this.blocked 	= false;
+		this.occopiedBy = undefined;
+
+	};
+
+	var createBattlefield = (function(){
+
+		for ( var y = 0; y < 10; y++ ) {
+			Battlefield.push( [] );
+			for ( var x = 0, row; x < 10; x++) {
+				Battlefield[y].push( new Field( y, x, 0 ) );
+			}
+		}
+
+	})();
+
+	var renderBattlefield = (function(){
+		
+		for (var row = 0, html = ""; row < Battlefield.length; row++ ) {
+			html += "<div class='row'>";
+			for ( var column = 0; column < Battlefield[row].length; column++ ) {
+				html += "<div class='field'>" + Battlefield[row][column].id + "</div>";
+			}
+			html += "</div>";
+		}
+
+		$battlefield.html(html);
+
+	})();
+
+	return Battlefield;
 
 })(jQuery);
+
+console.log( BOARD );
