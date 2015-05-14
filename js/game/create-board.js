b8e4991e8b7d28340f5a111ba9a1a1152c7ccdd8
Eric <e.mandersloot@gmail.com>
2015-05-15 01:43:20 +0200

    jo

---
+++ js/game/create-board.js
@@ -0,0 +1,57 @@
+/*=============================================================
+---------------------------------------------------------------
+===============================================================
+
+	Board constructor:
+	Creates and initiates the battlefield.
+
+===============================================================	
+---------------------------------------------------------------
+=============================================================*/
+
+var BOARD = (function( $ ){
+
+	var $battlefield 	= $("#battlefield"),
+		Battlefield 	= [];
+
+	var Field = function( y, x, id ){
+
+		this.row 		= y;
+		this.column 	= x;
+		this.id 		= String.fromCharCode(97 + y) + x;
+		this.occupied	= false;
+		this.blocked 	= false;
+		this.occopiedBy = undefined;
+
+	};
+
+	var createBattlefield = (function(){
+
+		for ( var y = 0; y < 10; y++ ) {
+			Battlefield.push( [] );
+			for ( var x = 0, row; x < 10; x++) {
+				Battlefield[y].push( new Field( y, x, 0 ) );
+			}
+		}
+
+	})();
+
+	var renderBattlefield = (function(){
+		
+		for (var row = 0, html = ""; row < Battlefield.length; row++ ) {
+			html += "<div class='row'>";
+			for ( var column = 0; column < Battlefield[row].length; column++ ) {
+				html += "<div class='field col-sm-1'>" + Battlefield[row][column].id + "</div>";
+			}
+			html += "</div>";
+		}
+
+		$battlefield.html(html);
+
+	})();
+
+	return Battlefield;
+
+})(jQuery);
+
+//console.log( BOARD );
---
+++ js/game/create-pieces.js
@@ -0,0 +1,119 @@
+/*=============================================================
+---------------------------------------------------------------
+===============================================================
+
+	Oieces constructor:
+	Creates and initiates the pieces.
+
+===============================================================	
+---------------------------------------------------------------
+=============================================================*/
+
+var PIECES = (function( $ ){
+
+	var Piece = function( name, rank, color ){
+
+		this.name 		= name;
+		this.rank 		= rank;
+		this.color		= color;
+		this.onBoard	= false;		
+		this.onField 	= undefined;
+		this.canMove	= (rank == "B" || rank == "F") ? false: true;
+		this.isScout	= (rank == 8) ? true : false;
+		this.isMiner 	= (rank == 7) ? true : false;
+		this.isSpy	 	= (rank == 9) ? true : false;
+		this.isBomb		= (rank == "B") ? true : false;
+		this.isFlag		= (rank == "F") ? true : false;
+
+	};
+
+	var startingpieces 	= ["Flag","Marshall","General","Colonel","Major","Captain","Sergeant","Miner","Scout","Spy","Bomb" ];
+	var redPieces 		= [];
+	var bluePieces 		= [];
+
+	var createPieces	= (function(){
+
+		for ( var x = 0, name, rank, quantity; x < startingpieces.length; x++ ) {
+
+			name = startingpieces[x];
+			quantity = 1;
+
+			switch (name) {
+
+				case "Flag":
+					rank = "F";
+					break;
+
+				case "Marshall":
+					rank = 1;
+					break;
+
+				case "General":
+					rank = 2;
+					break;
+
+				case "Colonel":
+					rank = 3, quantity = 2;
+					break;
+
+				case "Major":
+					rank = 4, quantity = 3;
+					break;
+
+				case "Captain":
+					rank = 5, quantity = 4;
+					break;
+
+				case "Sergeant":
+					rank = 6, quantity = 4;
+					break;
+
+				case "Miner":
+					rank = 7, quantity = 5;
+					break;
+
+				case "Scout":
+					rank = 8, quantity = 8;
+					break;
+
+				case "Spy":
+					rank = 9, quantity = 1;
+					break;
+
+				case "Bomb":
+					rank = "B", quantity = 6;
+					break;
+
+			}
+
+			for (i = 0; i < quantity; i++) {
+				redPieces.push( new Piece(name, rank, 'red') );	
+				bluePieces.push( new Piece(name, rank, 'blue') );	
+			}
+			
+		}
+
+	})();
+
+	var renderPieces = (function(){
+
+		for ( var x = 0; x < redPieces.length; x++ ) {
+
+			//hier kan ik de stukken rendenren
+			//zodra ik weet wat ik ermee ga doen
+
+		}
+
+	})();
+
+
+	return {
+
+		'red'  : redPieces,
+		'blue' : bluePieces
+
+	}
+
+
+})(jQuery);
+
